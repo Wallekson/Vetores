372995f7df9375cfe211581351b6057db3fcf096
Perkles <walisoncastor@gmail.com>
2023-06-21 15:31:16 -0300

    Exercise 11 completed

--- course/Exercicios/Exercicio11/dados_pessoas/src/main/java/application/Program.java
+++ course/Exercicios/Exercicio11/dados_pessoas/src/main/java/application/Program.java
@@ -12,10 +12,10 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n;
-        double menorAltura;
+        int n, quantMulher = 0, quantHomem = 0;
+        double menorAltura, maiorAltura, somaMulheres = 0.0, mediaMulheres;
 
-        System.out.println("Quantas pessoas serao digitadas?");
+        System.out.print("Quantas pessoas serao digitadas? ");
         n = sc.nextInt();
 
         Users[] users = new Users[n];
@@ -23,18 +23,48 @@ public class Program {
         for (int i = 0; i < n; i++){
             System.out.printf("Altura da %da pessoa: ", i + 1);
             double altura = sc.nextDouble();
-            System.out.printf("Altura da %da pessoa: ", i + 1);
+            System.out.printf("Genero da %da pessoa: ", i + 1);
             char genero = sc.next().charAt(0);
             users[i] = new Users (altura,genero);
         }
 
 
+        menorAltura = users[0].getAltura();
         for (int i = 0; i < n; i++){
-            if (users[i].getAltura() <  users[0]){
+            if (menorAltura > users[i].getAltura()){
+                menorAltura = users[i].getAltura();
 
             }
         }
 
+        maiorAltura = users[0].getAltura();
+        for (int i = 0; i < n; i++){
+            if (maiorAltura < users[i].getAltura()){
+                maiorAltura = users[i].getAltura();
+
+            }
+        }
+
+
+        for (int i = 0; i < n; i++){
+            if (users[i].getGenero() == 'F'){
+                somaMulheres += users[i].getAltura();
+                quantMulher++;
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if (users[i].getGenero() == 'M'){
+                quantHomem++;
+            }
+        }
+
+        mediaMulheres = somaMulheres / quantMulher;
+
+        System.out.printf("Menor altura = %.2f\n", menorAltura);
+        System.out.printf("Maior altura = %.2f\n", maiorAltura);
+        System.out.printf(" Media das alturas das mulheres = %.2f\n", mediaMulheres);
+        System.out.print("Numero de homens = " + quantHomem);
 
 
 
